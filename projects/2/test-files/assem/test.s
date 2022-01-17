9d52073707e0078aa7f4406198412af30918bd8d
udf <hellpl4y@gmail.com>
2022-01-17 21:00:16 +0500

    [prj2] wip cpu.circ (tested on jumps)

--- projects/2/test-files/assem/test.s
+++ projects/2/test-files/assem/test.s
@@ -1,4 +1,8 @@
-test: addiu $t0, $0, 40
-addiu $t1, $0, 41
-bne $t0, $t1, test
-addiu $t0, $0, 90
+addiu $t0, $0, 40
+addiu $t1, $0, 4
+jal label1
+addiu $t0, $0, 40
+label1: addiu $t0, $0, 41
+addiu $t0, $0, 42
+jr $ra
+addiu $t0, $0, 43
